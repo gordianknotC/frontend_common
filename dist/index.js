f8d09a76c606b6361a9c6826919cd89b86c165e0
curtis <curtis@fillygaming888.com>
2022-05-28 22:38:10 +0800

    Update tsconfig

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+/**
+ *      B A S E
+ *
+ * */
+import { UnCaughtCondition, UncaughtEnumType, NotImplementedError, TypeMismatchError, InvalidUsage, UnExpectedRole } from "~/base/baseExceptions";
+/***
+ *
+ *    E X T E N D
+ *    B A S E
+ * */
+import { AssertMsg, AssertionError, assert, } from "~/utils/assert";
+import { isRefImpl, asEnum, getAccessibleProperties, asCascadeClass, asUnWrappedVueRefMap, UnWrappedVueRef, addStringMappingFromNumEnum, Is, } from "~/utils/typeInferernce";
+/**
+ *        M I X I N S
+ *
+ *
+ * */
+import { useBuiltIn } from "~/base/builtinTypes";
+// import {assertMsg as _assertMsg} from "~/utils/assert";
+// import {is as _is} from "~/utils/typeInferernce";
+// export const is: InterfaceIs = _is;
+// export const assertMsg: AssertMsg = _assertMsg;
+export { AssertionError, AssertMsg, InvalidUsage, Is, NotImplementedError, TypeMismatchError, UnCaughtCondition, UncaughtEnumType, UnExpectedRole, 
+//
+addStringMappingFromNumEnum, asCascadeClass, asEnum, assert, asUnWrappedVueRefMap, getAccessibleProperties, isRefImpl, UnWrappedVueRef, useBuiltIn, };
+//# sourceMappingURL=index.js.map
