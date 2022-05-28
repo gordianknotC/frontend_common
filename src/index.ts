2573e2fa728485de4a89a74e97d43599b2b992fa
curtis <curtis@fillygaming888.com>
2022-05-28 20:00:31 +0800

    Fix type

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,12 @@ import {
 
 import {
   AssertMsg, AssertionError, assert,
-  assertMsg as _assertMsg,
 } from "~/appCommon/extendBase/impls/utils/assert";
 
 import {
   isRefImpl, asEnum, getAccessibleProperties, asCascadeClass,
   asUnWrappedVueRefMap, UnWrappedVueRef, TUnWrapVueRef,
   addStringMappingFromNumEnum, Is, InterfaceIs,
-  is as _is,
 } from "~/appCommon/extendBase/impls/utils/typeInferernce";
 
 
@@ -41,8 +39,8 @@ import {
  *
  * */
 import {useBuiltIn} from "~/appCommon/base/builtinAddonsTypes";
-export const is: InterfaceIs = _is;
-export const assertMsg: typeof _assertMsg = _assertMsg;
+export {assertMsg} from "~/appCommon/extendBase/impls/utils/assert";
+export {is} from "~/appCommon/extendBase/impls/utils/typeInferernce";
 
 export {
   AssertionError,
