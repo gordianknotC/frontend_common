e2d9fbff7db8585be69b75ccc9ce5b8ab019abc3
curtis <curtis@fillygaming888.com>
2022-10-13 11:03:07 +0800

    update typedef

--- dist/vueMixins/common.js
+++ dist/vueMixins/common.js
@@ -1,3 +1,4 @@
+import { merge } from "merge-anything";
 import { computed, watch } from "../base/vueTypes";
 import { assert } from "../utils/assert";
 export class CommonMixin {
@@ -22,19 +23,45 @@ export class CommonMixin {
 }
 const container = {};
 const FACADE_KEY = Symbol();
-export function injectFacade(providers) {
+export function injectFacade(providers, mergeObj = false) {
     var _a;
-    //@ts-ignore
     (_a = container[FACADE_KEY]) !== null && _a !== void 0 ? _a : (container[FACADE_KEY] = {});
-    Object.keys(providers).forEach((prop) => {
-        //@ts-ignore
-        container[FACADE_KEY][prop] = providers[prop];
-    });
+    if (!mergeObj) {
+        Object.keys(providers).forEach((prop) => {
+            container[FACADE_KEY][prop] = providers[prop];
+        });
+    }
+    else {
+        Object.keys(providers).forEach((prop) => {
+            if (container[FACADE_KEY][prop]) {
+                container[FACADE_KEY][prop] = merge(container[FACADE_KEY][prop], providers[prop]);
+            }
+            else {
+                container[FACADE_KEY][prop] = providers[prop];
+            }
+        });
+    }
 }
-export function IFacade(mapping) {
+function getByPath(seg, obj) {
+    const first = seg[0];
+    const last = seg[1];
+    if (last.length == 1) {
+        return obj[first][last[0]];
+    }
+    else {
+        return getByPath([last[0], last.splice(1)], obj[first]);
+    }
+}
+function pathRoute(path, obj) {
+    const segment = path.split(".");
+    const pathObj = [segment[0], segment.splice(1)];
+    return getByPath(pathObj, obj);
+}
+export function IFacade() {
     return new Proxy({}, {
         get: function (target, name) {
-            // @ts-ignore
+            var _a;
+            (_a = container[FACADE_KEY]) !== null && _a !== void 0 ? _a : (container[FACADE_KEY] = {});
             const facade = container[FACADE_KEY];
             assert(facade[name] !== undefined, `key name "${name.toString()}" not found in facade`);
             return facade[name];
