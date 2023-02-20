42f30178a07953933d0ddcec273ee6e043a7da83
curtis <curtis@fillygaming888.com>
2023-02-20 17:13:26 +0800

    Add SequencedQueueConsumer

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -33,19 +33,19 @@ export abstract class IAsyncQueue<META> {
   abstract dequeue(option: {id: number|string, removeQueue: boolean}): Promise<any>;
   abstract dequeueByResult(option: {id: number|string, result: any}): void;
   abstract clearQueue(): void;
+  abstract remove(item: Completer<any, QueueItem<META>>, reject: boolean): void;
 }
 
-/** {inheritDoc IAsyncQueue} 
+/** {inheritDoc IAsyncQueue}
  * @typeParam META - QueueItem 的 meta 型別
 */
 export abstract class IQueueConsumer<META> {
   abstract queue: IAsyncQueue<META>;
   abstract feedRequest(request: () => Promise<any>):  Completer<any, QueueItem<META>>;
   abstract consumeAll(): Promise<any>;
-  abstract consumeAllWhen(condition: (item:  Completer<any, QueueItem<META>>)=>boolean): Promise<any>;
 }
 
-/** {inheritDoc IAsyncQueue} 
+/** {inheritDoc IAsyncQueue}
  * 應用如 api client 處理需籍由 websocket 傳送出去的請求, 將請求暫存於 queue 以後，待收到 socket
  * 資料，再由 queue 裡的 promise resolve 返回值， resolve 後無論成功失敗，移除該筆 queue
  * @typeParam META - {@link QueueItem} 的 meta 型別
@@ -90,7 +90,7 @@ export abstract class IQueueConsumer<META> {
       const resultA = await q.dequeue({id: idA});
       const resultB = await q.dequeue({id: idB});
       const resultC = await q.dequeue({id: idC});
-      
+
       expect(resultA).toEqual({idA});
       expect(resultB).toEqual({idB});
       expect(resultC).toEqual({idC});
@@ -127,8 +127,8 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
    * @param promise - 處理非同部請求邏輯，待請求完成後，queue 生命周期完成移除
    * @param timeout - default 10 * 1000
    * @param meta - 使用者自定義註解
-   * @param dequeueImmediately - enqueue 後馬上 dequeue，即執上 promise 直到 promise resolve 後
-   * @returns 
+   * @param dequeueImmediately - enqueue 後馬上 dequeue，即執行 promise 直到 promise resolve 後
+   * @returns
    * @example - 1
       ```ts
       q.enqueue(idC, async ()=>{
@@ -174,7 +174,7 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
     return completer;
   }
 
-  /** 與  {@link enqueue} 相同，只是 id 自動生成 
+  /** 與  {@link enqueue} 相同，只是 id 自動生成
    * @returns Completer 物件，非 async Promise
   */
   public enqueueWithoutId(
@@ -201,14 +201,14 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
     return this.timeoutErrorObj;
   }
 
-  private remove(item: Completer<any, QueueItem<META>>, reject: boolean = false) {
+  remove(item: Completer<any, QueueItem<META>>, reject: boolean = false) {
     clearTimeout(item._meta.timeout);
-    if (reject)
+    if (reject) {
       item.reject({
         reason: "flushed"
       })
+    }
     this.queue.remove(item);
-
     console.log("remove:", item._meta.id);
   }
 
@@ -222,14 +222,14 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
 
 
   /**
-   * 別於 {@link dequeue} 執行 {@link enqueue} 傳入的 promise 方法，待 promise 請求 
+   * 別於 {@link dequeue} 執行 {@link enqueue} 傳入的 promise 方法，待 promise 請求
    * resolve 後移除 {@link QueueItem}, {@link dequeueByResult} 則是不管 {@link enqueue}
    * 所傳入的 promise 結果，直接取代其 result
-   * 
+   *
    * @param option.id - 取得queue的id
    * @param option.result - sync result 與 async result 二擇一
    * @param option.asyncResult - async result 與 sync result 二擇一
-   * @returns 
+   * @returns
    * @example
    ```ts
     const pendingId = "idA";
@@ -237,7 +237,7 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
     const pending = q.enqueue(pendingId, async ()=>{
         return waitForTimeOut(10 * 1000);
     });
-    // 覆寫內容於是能將值返回給 pending 
+    // 覆寫內容於是能將值返回給 pending
     q.dequeueByResult({id: pendingId, result: {succeed: true}});
     expect(pending).resolves.toEquals({succeed: true});
    ```
@@ -271,7 +271,7 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
       }, timeout)
       completer.complete("hello");
       expect(Q.queue.length).toBe(1)
-      
+
       Q.dequeueByResult({id: 123, result: "999"});
       expect(completer.future).resolves.toEqual("hello")
       expect(Q.queue.length).toBe(0)
@@ -314,7 +314,7 @@ export class AsyncQueue<META=any> implements IAsyncQueue<META> {
    * option.removeQueue 決定是否移除 QueueItem, 預設 option.removeQueue 為 true
    * @param option.id - 取得queue的id
    * @param option.removeQueue - 預設 true
-   * @returns 
+   * @returns
    */
   public async dequeue(option: {id: number|string, removeQueue?: boolean}): Promise<any> {
     const {id, removeQueue} = option;
@@ -352,10 +352,22 @@ export class SequencedQueueConsumer <META>
     return item;
   }
 
-  consumeAll(): Promise<any> {
-    throw new Error("Method not implemented.");
+  async consumeAll(): Promise<any> {
+    return this.consumeWhen((_)=>true);
   }
-  consumeAllWhen(condition: (item: Completer<any, QueueItem<any>>) => boolean): Promise<any> {
-    throw new Error("Method not implemented.");
+
+  protected async consumeWhen(condition: (item: Completer<any, QueueItem<any>>) => boolean): Promise<any> {
+    const qItems = this.queue.queue.filter(condition);
+    const result = [];
+    for (let i = 0; i < qItems.length; i++) {
+      const futureItem = qItems[i].future;
+      result.push(futureItem);
+      await (futureItem.catch((_)=>{
+        return _;
+      }));
+      this.queue.remove(qItems[i], false);
+    }
+    this.queue.clearQueue();
+    return result;
   }
 }
